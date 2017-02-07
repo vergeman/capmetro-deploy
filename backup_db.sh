48a601276261234d2c2b94cc533479caddbaf6fc
vergeman <alan.verga@gmail.com>
2017-02-06 23:38:30 -0600

    remove mongodump, use file system bkup

--- backup_db.sh
+++ backup_db.sh
@@ -13,6 +13,7 @@ HOST=`AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY ec
 
 # DB name
 DBNAME=capmetro
+DBNAME_DIR=/mnt/data/mongodb
 
 # S3 bucket name & region
 BUCKET=capmetro-db-$ENV
@@ -22,12 +23,16 @@ REGION=$REGION
 USER=ubuntu
 
 # Current time
-TIME=`/bin/date +%d-%m-%Y-%T`
+TIME=`/bin/date +%d-%m-%Y`
 
 # Backup directory
 DEST=/mnt/data/capmetro_prod_db_bkup
 
-BKUP_FILE=$DBNAME.$TIME.gz
+BKUP_FILE=$DBNAME.$TIME.tar.gz
+
+#stop any running process
+echo "Stopping capmetro crawl process"
+echo `ssh -i $KEYFILE $USER@$HOST "cd /home/$USER/capmetro-crawl/ && npm run stop"`
 
 #Create backup dir (-p to avoid warning if already exists)
 `ssh -i $KEYFILE $USER@$HOST "sudo /bin/mkdir -p $DEST"`
@@ -35,8 +40,8 @@ BKUP_FILE=$DBNAME.$TIME.gz
 # Log
 echo "Backing up $HOST/$DBNAME to s3://$BUCKET/ on $TIME";
 
-# Dump from mongodb host into backup directory on prod instance (better bandwith ec2 -> s3)
-echo `ssh -i $KEYFILE $USER@$HOST "sudo /usr/bin/mongodump -d $DBNAME --archive=$DEST/$BKUP_FILE --gzip"`
+# Dump from mongodb dir into backup directory on prod instance (better bandwith ec2 -> s3)
+echo `ssh -i $KEYFILE $USER@$HOST "sudo tar cvfz $DEST/$BKUP_FILE $DBNAME_DIR"`
 
 # Upload archive to s3
 echo `ssh -i $KEYFILE $USER@$HOST "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY /usr/bin/aws s3 cp --region $REGION $DEST/$BKUP_FILE s3://$BUCKET/"`
@@ -47,6 +52,9 @@ echo `ssh -i $KEYFILE $USER@$HOST "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECR
 # Remove backup directory
 `ssh -i $KEYFILE $USER@$HOST "sudo /bin/rm -rf $DEST"`
 
+#restart capmetro crawl
+echo `ssh -i $KEYFILE $USER@$HOST "cd /home/$USER/capmetro-crawl/ && npm run prod"`
+
 # All done
 echo "Backup available at https://s3.amazonaws.com/$BUCKET/$BKUP_FILE"
-echo "Restore: 'sudo mongorestore -d $DBNAME --archive=$DEST/$BKUP_FILE --gzip' "
+echo "Restore: 'tar xvfz $BKUP_FILE'"
