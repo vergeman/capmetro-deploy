5b808795d1af5f78ceafc00e6b47fa09aaf5d1a4
vergeman <alan.verga@gmail.com>
2017-04-22 14:06:32 -0500

    added logrotate task, included tasks args in bash helper

--- ec2_playbook.sh
+++ ec2_playbook.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -xe
 
 : ${ENV:?"Need to set ENV"}
-ansible-playbook ec2_playbook.yml -i ec2.py -vvv --private-key=~/.ssh/capmetro-deploy.pem --extra-vars "env=$ENV"
+
+#add tag arg
+if [ -z ${TAGS} ]; then TAGS=all; else echo "limiting tasks to: $TAGS"; fi
+
+ansible-playbook ec2_playbook.yml -i ec2.py -vvv --private-key=~/.ssh/capmetro-deploy.pem --tags="$TAGS" --extra-vars "env=$ENV"
